206487c14a1ecc150f474d3c95b734be8cf60034
Mina-tech <minacorovic578@gmail.com>
2020-05-28 09:57:31 +0200

    invoices page

---
+++ ProductApp/ClientApp/src/components/Invoices - Copy.js
@@ -0,0 +1,74 @@
+﻿import React, { Component } from 'react';
+import { Form } from 'react-bootstrap';
+
+
+
+
+
+
+export class Invoices extends Component {
+    static displayName = Invoices.name;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            invoice: [], loading: true
+        }
+    }
+
+        componentDidMount() {
+            this.GetInvoice();
+        }
+
+        
+    static getInvoices(invoices) {
+        return (
+            <div class="container">
+                
+                
+                <ul>
+                    <li>
+                        <input list="invoiceType" placeholder="Tip profakture" />
+                        <datalist id="invoiceType">
+                            {invoices.map(invoice =>
+                                <option>{invoice.invoiceType}</option>
+                            )}
+                        </datalist>
+                        <p>----------------------------------------------</p>
+                    </li>
+                    <li>
+                        <input list="invoiceAddress" placeholder="Adresa firme" />
+                        <datalist id="invoiceAddress">
+                            {invoices.map(invoice =>
+                                <option>{invoice.address}</option>
+                            )}
+                        </datalist>
+                        <p>----------------------------------------------</p>
+                    </li>
+                </ul>
+            </div>
+
+          
+            
+           
+        );
+    }
+        render() {
+            let contents = this.state.loading
+                ? <p><em>Loading...</em></p>
+                : Invoices.getInvoices(this.state.invoice);
+            return (
+                <div>
+                    <p>Invoices</p>
+                    {contents}
+                </div>
+            );
+        }
+
+        async GetInvoice() {
+            const response = await fetch('/Home/GetAllInvoices');
+            const data = await response.json();
+            this.setState({ invoice: data, loading: false });
+        }
+
+    }
--- ProductApp/ClientApp/src/components/Invoices.js
+++ ProductApp/ClientApp/src/components/Invoices.js
@@ -1,10 +1,6 @@
 ﻿import React, { Component } from 'react';
+import './Invoices.css';
 import { Form } from 'react-bootstrap';
-
-
-
-
-
 
 export class Invoices extends Component {
     static displayName = Invoices.name;
@@ -24,63 +20,154 @@ export class Invoices extends Component {
     static getInvoices(invoices) {
         return (
             <div class="container">
-                
-                
-                 <ul>
-                    {
-                        invoices.map(invoice =>
-                            <li key={invoice.invoiceId}>
+                <ul className="fakture">
+                    <li>
+                        <input list="invoiceType" placeholder="Tip profakture" />
+                        <datalist id="invoiceType">
+                            {invoices.map(invoice =>
+                                <option>{invoice.invoiceType}</option>
+                            )}
+                        </datalist>
+                    </li>
+                    <li>
+                        <input list="invoiceId" placeholder="Broj profakture" />
+                        <datalist id="invoiceId">
+                            {invoices.map(invoice =>
+                                <option>{invoice.invoiceId}</option>
+                            )}
+                        </datalist>
+                    </li>
+                    <li>
+                        <input list="distributorId" placeholder="Naziv firme" />
+                        <datalist id="distributorId">
+                            {invoices.map(invoice =>
+                                <option>{invoice.distributorId}</option>
+                            )}
+                        </datalist>
+                    </li>
+                    <li>
+                        <input list="invoiceDate" placeholder="Datum profakture" />
+                        <datalist id="invoiceDate">
+                            {invoices.map(invoice =>
+                                <option>{new Date(invoice.invoiceDate).getMonth()}/{new Date(invoice.invoiceDate).getDate()}/{new Date(invoice.invoiceDate).getFullYear()}</option>
+                            )}
+                        </datalist>
+                    </li>
+                    <li>
+                        <input list="invoiceAddress" placeholder="Adresa firme" />
+                        <datalist id="invoiceAddress">
+                            {invoices.map(invoice =>
+                                <option>{invoice.address}</option>
+                            )}
+                        </datalist>
+                    </li>
+                    <li>
+                        <input list="invoiceDeliveryDate" placeholder="Datum isporuke" />
+                        <datalist id="invoiceDeliveryDate">
+                            {invoices.map(invoice =>
+                                <option>{new Date(invoice.invoiceDeliveryDate).getMonth()}/{new Date(invoice.invoiceDeliveryDate).getDate()}/{new Date(invoice.invoiceDeliveryDate).getFullYear()}</option>
+                            )}
+                        </datalist>
+                    </li>
+                    <li>
+                        <input list="invoiceCity" placeholder="Grad" />
+                        <datalist id="invoiceCity">
+                            {invoices.map(invoice =>
+                                <option>{invoice.city}</option>
+                            )}
+                        </datalist>
+                    </li>
+                    <li>
+                        <input list="invoicePaymentDate" placeholder="Datum plaćanja" />
+                        <datalist id="invoicePaymentDate">
+                            {invoices.map(invoice =>
+                                <option>{new Date(invoice.invoicePaymentDate).getMonth()}/{new Date(invoice.PaymentDate).getDate()}/{new Date(invoice.PaymentDate).getFullYear()}</option>
+                            )}
+                        </datalist>
+                    </li>
+                    <li>
+                        <input list="invoiceCountry" placeholder="Država" />
+                        <datalist id="invoiceCountry">
+                            {invoices.map(invoice =>
+                                <option>{invoice.country}</option>
+                            )}
+                        </datalist>
+                    </li>
+                    <li>
+                        <input list="invoiceMethodOfDelivery" placeholder="Način isporuke" />
+                        <datalist id="invoiceMethodOfDelivery">
+                            {invoices.map(invoice =>
+                                <option>{invoice.methodOfDelivery}</option>
+                            )}
+                        </datalist>
+                    </li>
+                    <li>
+                        <input list="invoiceContactPerson" placeholder="Kontakt osoba" />
+                        <datalist id="invoiceContactPerson">
+                            {invoices.map(invoice =>
+                                <option>{invoice.contactPerson}</option>
+                            )}
+                        </datalist>
+                    </li>
+                    <li>
+                        <input list="invoicePlaceOfDelivery" placeholder="Mesto isporuke" />
+                        <datalist id="invoicePlaceOfDelivery">
+                            {invoices.map(invoice =>
+                                <option>{invoice.placeOfDelivery}</option>
+                            )}
+                        </datalist>
+                    </li>
+                    <li>
+                        <input list="invoiceContactHomeNumber" placeholder="Kontakt telefon - fiksni" />
+                        <datalist id="invoiceContactHomeNumber">
+                            {invoices.map(invoice =>
+                                <option>{invoice.contactHomeNumber}</option>
+                            )}
+                        </datalist>
+                    </li>
+                    
+                    <li>
+                        <input list="invoiceEmail" placeholder="Email adresa" />
+                        <datalist id="invoiceEmail">
+                            {invoices.map(invoice =>
+                                <option>{invoice.email}</option>
+                            )}
+                        </datalist>
+                    </li>
+                    <li>
+                        <input list="invoiceContactMobileNumber" placeholder="Kontakt telefon - mobilni" />
+                        <datalist id="invoiceContactMobileNumber">
+                            {invoices.map(invoice =>
+                                <option>{invoice.contactMobileNumber}</option>
+                            )}
+                        </datalist>
+                    </li>
+                    <li>
+                        <input list="invoiceWebAddress" placeholder="Web adresa" />
+                        <datalist id="invoiceWebAddress">
+                            {invoices.map(invoice =>
+                                <option>{invoice.webAddress}</option>
+                            )}
+                        </datalist>
+                    </li>
+                    <li>
+                        <Form>
+                            <Form.Group controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Tekst polje za komentar</Form.Label>
+                                <Form.Control as="textarea" rows="3" />
+                            </Form.Group>
+                        </Form>
+                    </li>
 
-                                <Form>
-                                    <Form.Group controlId="exampleForm.SelectCustom">
-                                        <Form.Label><input list="invoices" id="invoiceId" name="invoiceNumber" placeholder="Tip profakture" /></Form.Label>
-                                        <datalist id="invoices">
-
-                                            
-
-                                                <option>{invoice.invoiceId}</option>
-
-                                            </datalist>
-                                      
-                                      
-                                    </Form.Group>
-                                </Form>
-
-
-                               
-                              
-                            
-                                
-                                <p>{invoice.city}</p>
-                                <p>{invoice.country}</p>
-                                <p>{invoice.contactPerson}</p>
-                                <p>{invoice.contactPersonJobPosition}</p>
-                                <p>{invoice.contactHomeNumber}</p>
-                                <p>{invoice.contactMobileNumber}</p>
-                                <form>
-                                    <div class="form-group">
-                                        <label for="exampleInputEmail1">Email address</label>
-                                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={invoice.email} />
-                                    </div>
-                                </form>
-                                <p>{invoice.webAddress}</p>
-
-                                
-
-                                <p>{invoice.invoiceId}</p>
-                                <p>{invoice.invoiceDate}</p>
-                                <p>{invoice.products}</p>
-                                <p>{invoice.distributorId}</p>
-                                <p>{invoice.invoiceNumber}</p>
-                                <p>{invoice.deliveryDate}</p>
-                                <p>{invoice.paymentDate}</p>
-                                <p>{invoice.methodOfDelivery}</p>
-                                <p>{invoice.placeOfDelivery}</p>
-                               
-                                  
-                            </li>
-                        )
-                    }
+                    <li>
+                        <Form>
+                            <Form.Group controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Tekst polje za komentar</Form.Label>
+                                <Form.Control as="textarea" rows="3" />
+                            </Form.Group>
+                        </Form>
+                    </li>
+                   
                 </ul>
             </div>
 
